10c5fbe4b292b900c1d611a82cb3d6e52ef5589d
rayenbakali <rayen.bakali@gmail.com>
2022-03-07 18:20:24 +0100

    mail modifications

--- desktop/src/tn/nebulagaming/utils/JavaMail.java
+++ desktop/src/tn/nebulagaming/utils/JavaMail.java
@@ -4,23 +4,31 @@
  */
 package tn.nebulagaming.utils;
 
+import java.io.File;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Authenticator;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import tn.nebulagaming.config.MailConfig;
 
 /**
  *
- * @author ASUS
+ * @author Aymen Dhahri
  */
 public class JavaMail {
 
@@ -30,64 +38,110 @@ public class JavaMail {
 
     public JavaMail() {
 
-        CONF = MailConfig.getInstance().getConfig();
-        PROPS = new Properties();
+    CONF = MailConfig.getInstance().getConfig();
+    PROPS = new Properties();
 
-        PROPS.setProperty("mail.smtp.auth", "true");
-        PROPS.setProperty("mail.smtp.starttls.enable", "true");
-        PROPS.setProperty("mail.smtp.host", CONF.get("HOST"));
-        PROPS.setProperty("mail.smtp.port", CONF.get("PORT"));
-        PROPS.setProperty("mail.default-encoding", "UTF-8");
+    PROPS.setProperty("mail.smtp.auth", "true");
+    PROPS.setProperty("mail.smtp.starttls.enable", "true");
+    PROPS.setProperty("mail.smtp.host", CONF.get("HOST"));
+    PROPS.setProperty("mail.smtp.port", CONF.get("PORT"));
+    PROPS.setProperty("mail.default-encoding", "UTF-8");
 
-        SESSION = Session.getDefaultInstance(PROPS, new Authenticator() {
+    SESSION = Session.getDefaultInstance(PROPS, new Authenticator() {
 
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
 
-                return new PasswordAuthentication(CONF.get("EMAIL"), CONF.get("PWD"));
-            }
-        });
+        return new PasswordAuthentication(CONF.get("EMAIL"), CONF.get("PWD"));
+        }
+    });
+    }
+
+    public void sendEmailWithAttach(String subject, String to, String body, File f) {
+
+    System.out.println("Preparing to send email...");
+
+    try {
+
+        MimeMessage attch = prepareMailWithAttach(to, subject, body, f);
+
+        Transport.send(attch);
+
+        System.out.println("Message sent successfully !!");
+
+    } catch (MessagingException ex) {
+        System.out.println(ex.getMessage());
+    }
     }
 
     public void sendTextMail(String subject, String to, String body) {
 
-        System.out.println("Preparing to send email...");
+    System.out.println("Preparing to send email...");
 
-        try {
+    try {
 
-            MimeMessage textMail = createTextMessage(to, subject, body);
+        MimeMessage textMail = createTextMessage(to, subject, body);
 
-            Transport.send(textMail);
-            System.out.println("Message sent successfully !!");
+        Transport.send(textMail);
 
-        } catch (MessagingException ex) {
-            System.out.println(ex.getMessage());
-        }
+        System.out.println("Message sent successfully !!");
 
+    } catch (MessagingException ex) {
+        System.out.println(ex.getMessage());
+    }
+
+    }
+
+    private MimeMessage prepareMailWithAttach(String to, String subject, String body, File f) throws AddressException, MessagingException {
+
+    MimeMessage message = prepareMessage(to, subject);
+
+    BodyPart messageBodyPart = new MimeBodyPart();
+
+    Multipart multipart = new MimeMultipart();
+
+    messageBodyPart.setText(body);
+
+    multipart.addBodyPart(messageBodyPart);
+
+    messageBodyPart = new MimeBodyPart();
+
+    DataSource source = new FileDataSource(f.getAbsolutePath());
+
+    System.out.println(f.getAbsolutePath());
+
+    messageBodyPart.setDataHandler(new DataHandler(source));
+
+    messageBodyPart.setFileName(f.getName());
+
+    multipart.addBodyPart(messageBodyPart);
+
+    message.setContent(multipart);
+
+    return message;
     }
 
     private MimeMessage createTextMessage(String to, String subject, String body) throws MessagingException {
 
-        MimeMessage message = prepareMessage(to, subject);
+    MimeMessage message = prepareMessage(to, subject);
 
-        message.setSentDate(new Date());
+    message.setSentDate(new Date());
 
-        message.setText(body);
+    message.setText(body);
 
-        return message;
+    return message;
     }
 
     private MimeMessage prepareMessage(String to, String subject) throws AddressException, MessagingException {
-        MimeMessage message = new MimeMessage(SESSION);
 
-        message.setFrom(new InternetAddress(CONF.get("EMAIL")));
+    MimeMessage message = new MimeMessage(SESSION);
 
-        // Set To: header field of the header.
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+    message.setFrom(new InternetAddress(CONF.get("EMAIL")));
 
-        // Set Subject: header field
-        message.setSubject(subject);
+    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
 
-        return message;
+    message.setSubject(subject);
+
+    return message;
     }
 }
