23676f8ac6b2c857cd26f923cb6fe02939459bdd
AzizMakni <mohamedaziz.makni@esprit.tn>
2022-05-11 18:05:44 +0100

    mobile commit

---
+++ mobile/src/JV/gui/ListJeuForm.java
@@ -0,0 +1,267 @@
+/*
+ * Copyright (c) 2016, Codename One
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ */
+
+package JV.gui;
+
+
+import JV.gui.AjoutJeuForm;
+import com.codename1.components.ImageViewer;
+import com.codename1.components.ScaleImageLabel;
+import com.codename1.components.SpanLabel;
+import com.codename1.components.ToastBar;
+import com.codename1.ui.Button;
+import com.codename1.ui.ButtonGroup;
+import com.codename1.ui.Command;
+import com.codename1.ui.Component;
+import static com.codename1.ui.Component.BOTTOM;
+import static com.codename1.ui.Component.CENTER;
+import static com.codename1.ui.Component.RIGHT;
+import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Display;
+import com.codename1.ui.EncodedImage;
+import com.codename1.ui.Font;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Graphics;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.RadioButton;
+import com.codename1.ui.Slider;
+import com.codename1.ui.Tabs;
+import com.codename1.ui.TextArea;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.URLImage;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.geom.Dimension;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.layouts.GridLayout;
+import com.codename1.ui.layouts.LayeredLayout;
+import com.codename1.ui.plaf.Border;
+import com.codename1.ui.plaf.Style;
+import com.codename1.ui.util.Resources;
+import com.company.entities.JeuVideo;
+import com.mycompany.gui.BaseForm;
+import com.mycompany.services.ServiceJeuVideo;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * The newsfeed form
+ *
+ * @author Shai Almog
+ */
+public class ListJeuForm extends BaseForm {
+        Form current;
+           ImageViewer imgv;
+
+    public ListJeuForm(Resources res) {
+        super("Jeux video", BoxLayout.y());
+        Toolbar tb = new Toolbar(true);
+        setToolbar(tb);
+        getTitleArea().setUIID("Container");
+        setTitle("Jeux video");
+        getContentPane().setScrollVisible(false);
+        
+        super.addSideMenu(res);
+        tb.addSearchCommand(e -> {});
+        
+        Tabs swipe = new Tabs();
+
+        Label spacer1 = new Label();
+        addTab(swipe, res.getImage("Capture.PNG"), spacer1, "  ", "", " ");
+                
+        swipe.setUIID("Container");
+        swipe.getContentPane().setUIID("Container");
+        swipe.hideTabs();
+        
+        ButtonGroup bg = new ButtonGroup();
+        int size = Display.getInstance().convertToPixels(1);
+        Image unselectedWalkthru = Image.createImage(size, size, 0);
+        Graphics g = unselectedWalkthru.getGraphics();
+        g.setColor(0xffffff);
+        g.setAlpha(100);
+        g.setAntiAliased(true);
+        g.fillArc(0, 0, size, size, 0, 360);
+        Image selectedWalkthru = Image.createImage(size, size, 0);
+        g = selectedWalkthru.getGraphics();
+        g.setColor(0xffffff);
+        g.setAntiAliased(true);
+        g.fillArc(0, 0, size, size, 0, 360);
+        RadioButton[] rbs = new RadioButton[swipe.getTabCount()];
+        FlowLayout flow = new FlowLayout(CENTER);
+        flow.setValign(BOTTOM);
+        Container radioContainer = new Container(flow);
+        for(int iter = 0 ; iter < rbs.length ; iter++) {
+            rbs[iter] = RadioButton.createToggle(unselectedWalkthru, bg);
+            rbs[iter].setPressedIcon(selectedWalkthru);
+            rbs[iter].setUIID("Label");
+            radioContainer.add(rbs[iter]);
+        }
+                
+        rbs[0].setSelected(true);
+        swipe.addSelectionListener((i, ii) -> {
+            if(!rbs[ii].isSelected()) {
+                rbs[ii].setSelected(true);
+            }
+        });
+        
+        Component.setSameSize(radioContainer, spacer1);
+        add(LayeredLayout.encloseIn(swipe, radioContainer));
+                                    
+        Button Ajouter = new Button("Ajouter");
+        Ajouter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                    new AddJV(res).show();
+            }
+        });
+        add(Ajouter);
+        
+        ButtonGroup barGroup = new ButtonGroup();
+                  Container co=new Container(BoxLayout.xCenter());;
+                    ArrayList <JeuVideo> jvs = new ArrayList();
+                    ServiceJeuVideo sa =new ServiceJeuVideo();
+                    jvs=sa.getAllJeu();
+
+                 for (JeuVideo fi : jvs) {
+                            Container ct = new Container(BoxLayout.y());
+                            String url = "file://D:/WEB_Symfony/Forum/Front/public/uploads/images/"+ fi.getImagevg();
+                            int deviceWidth = Display.getInstance().getDisplayWidth();
+                            Image placeholder = Image.createImage( deviceWidth/3,  deviceWidth/3, 0xbfc9d2);
+                            EncodedImage encImage = EncodedImage.createFromImage(placeholder, false);
+                            Image i = URLImage.createToStorage(encImage, "fileNameInStoragez" + fi.getImagevg(),url, URLImage.RESIZE_SCALE);
+                                imgv = new ImageViewer();
+                                imgv.setImage(i);
+                                ct.add(imgv);
+
+                            Label l = new Label("ID : "+fi.getId());
+                            Label l2 = new Label("Name : "+fi.getNamevg(),"RedLabel");
+                            //Label l3 = new Label("Likes : "+fi.getLikes(),"SmallLabel");
+                            Slider SNN = createStarRankSlider();
+                            SNN.setProgress((int) fi.getRating());
+                            SNN.setEditable(false);
+
+
+                            l2.getAllStyles().setFgColor(0xf15f5f);
+                            ct.add(l);
+                            ct.add(l2);
+                            //ct.add(l3);
+                            add(FlowLayout.encloseCenter(SNN));
+                            Button Modif = new Button("Modifier");
+                            Button Supprimer = new Button("Supprimer");
+                            Modif.addActionListener(new ActionListener() {
+                                            @Override
+            public void actionPerformed(ActionEvent evt) {               
+                                 //new ModifierReclamation(res,current,fi).show();                   
+                                                    }   
+                                            });
+                            Supprimer.addActionListener(new ActionListener() {
+                                            @Override
+            public void actionPerformed(ActionEvent evt) {               
+                if (Dialog.show("Confirmation", "Voulez vous supprimer ce jeu ?", "Oui", "Annuler")) {
+
+                  if( ServiceJeuVideo.getInstance().deleteJV(fi.getId()))
+                            {
+                                Dialog.show("Success","Jeu supprimé!",new Command("OK"));
+                                new ListJeuForm(res).show();
+                            }
+
+                            }
+                   
+                }   
+        });
+                       ct.add(Modif);
+                       ct.add(Supprimer);
+
+
+                       Label separator = new Label("","Separator");
+                       ct.add(separator);
+                       add(ct);
+               }
+    }
+        
+    private void addTab(Tabs swipe, Image img, Label spacer, String likesStr, String commentsStr, String text) {
+        int size = Math.min(Display.getInstance().getDisplayWidth(), Display.getInstance().getDisplayHeight());
+        if(img.getHeight() < size) {
+            img = img.scaledHeight(size);
+        }
+        Label likes = new Label(likesStr);
+        Style heartStyle = new Style(likes.getUnselectedStyle());
+        heartStyle.setFgColor(0xff2d55);
+        FontImage heartImage = FontImage.createMaterial(FontImage.MATERIAL_FAVORITE, heartStyle);
+        likes.setIcon(heartImage);
+        likes.setTextPosition(RIGHT);
+
+        Label comments = new Label(commentsStr);
+        FontImage.setMaterialIcon(comments, FontImage.MATERIAL_CHAT);
+        if(img.getHeight() > Display.getInstance().getDisplayHeight() / 2) {
+            img = img.scaledHeight(Display.getInstance().getDisplayHeight() / 2);
+        }
+        ScaleImageLabel image = new ScaleImageLabel(img);
+        image.setUIID("Container");
+        image.setBackgroundType(Style.BACKGROUND_IMAGE_SCALED_FILL);
+        Label overlay = new Label(" ", "ImageOverlay");
+        
+        Container page1 = 
+            LayeredLayout.encloseIn(
+                image,
+                overlay,
+                BorderLayout.south(
+                    BoxLayout.encloseY(
+                            new SpanLabel(text, "LargeWhiteText"),
+                            spacer
+                        )
+                )
+            );
+
+        swipe.addTab("", page1);
+    }
+    
+         private Slider createStarRankSlider() {
+    Slider starRank = new Slider();
+    starRank.setEditable(true);
+    starRank.setMinValue(0);
+    starRank.setMaxValue(5);
+    Font fnt = Font.createTrueTypeFont("native:MainLight", "native:MainLight").
+            derive(Display.getInstance().convertToPixels(5, true), Font.STYLE_PLAIN);
+    Style s = new Style(0xffff33, 0, fnt, (byte)0);
+    Image fullStar = FontImage.createMaterial(FontImage.MATERIAL_STAR, s).toImage();
+    s.setOpacity(100);
+    s.setFgColor(0);
+    Image emptyStar = FontImage.createMaterial(FontImage.MATERIAL_STAR, s).toImage();
+    initStarRankStyle(starRank.getSliderEmptySelectedStyle(), emptyStar);
+    initStarRankStyle(starRank.getSliderEmptyUnselectedStyle(), emptyStar);
+    initStarRankStyle(starRank.getSliderFullSelectedStyle(), fullStar);
+    initStarRankStyle(starRank.getSliderFullUnselectedStyle(), fullStar);
+    starRank.setPreferredSize(new Dimension(fullStar.getWidth() * 5, fullStar.getHeight()));
+    return starRank;
+}
+     private void initStarRankStyle(Style s, Image star) {
+    s.setBackgroundType(Style.BACKGROUND_IMAGE_TILE_BOTH);
+    s.setBorder(Border.createEmpty());
+    s.setBgImage(star);
+    s.setBgTransparency(0);
+}
+    
+    }
---
+++ mobile/src/com/mycompany/services/ServiceJeuVideo.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.services;
+
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionListener;
+import com.company.entities.JeuVideo;
+import com.mycompany.utils.Statics;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author dell
+ */
+public class ServiceJeuVideo {
+    public ArrayList<JeuVideo>List1;
+        public boolean resultOK;
+
+    public static ServiceJeuVideo instance = null;
+    
+    private ConnectionRequest req;
+    
+    public static ServiceJeuVideo getInstance()
+    {
+        if(instance == null)
+            instance = new ServiceJeuVideo();
+        return instance;
+    }
+    
+    public ServiceJeuVideo()
+    {
+        req = new ConnectionRequest();
+    }
+    
+    public void ajoutJeuVideo(JeuVideo JV)
+    {
+        String url = Statics.BASE_URL+"/jeu/mobile/new?name="+JV.getNamevg()+"&image="+JV.getImagevg()+"&rating="+JV.getRating();
+        
+        req.setUrl(url);
+        req.addResponseListener((e)-> {
+            String str = new String(req.getResponseData());
+            System.out.println("data=="+str);
+        }
+            );
+        
+        NetworkManager.getInstance().addToQueueAndWait(req);
+    }
+    
+    public ArrayList<JeuVideo>afficherJeuVideo()
+    {
+        ArrayList<JeuVideo> result = new ArrayList<>();
+        
+        String url = Statics.BASE_URL;
+        req.setUrl(url);
+        
+        req.addResponseListener(new ActionListener<NetworkEvent> ()
+        {
+            @Override
+            public void actionPerformed(NetworkEvent evt)
+            {
+                JSONParser jsonp;
+                jsonp = new JSONParser();
+                
+                try{
+                    Map<String, Object>mapJeuVideo = jsonp.parseJSON(new CharArrayReader(new String(req.getResponseData()).toCharArray()));
+                    List<Map<String, Object>> listOfMaps = (List<Map<String, Object>>) mapJeuVideo.get("root");
+                    
+                    for(Map<String, Object> obj : listOfMaps)
+                    {
+                        JeuVideo jv = new JeuVideo();
+                        int id = (int) Float.parseFloat(obj.get("id").toString());
+                        jv.setId(id);
+                        String name = obj.get("namevg").toString();
+                        String image = obj.get("imagevg").toString();
+                        jv.setRating((int) Float.parseFloat(obj.get("rating").toString()));
+                        jv.setLikes((int) Float.parseFloat(obj.get("likes").toString()));
+                        jv.setNamevg(name);
+                        jv.setImagevg(image);
+                        
+                        result.add(jv);
+                        
+                    }
+                }catch(Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+        });
+            
+        NetworkManager.getInstance().addToQueueAndWait(req);
+                
+        return result;
+
+    }
+    
+    public ArrayList<JeuVideo> parseJeu(String jsonText)
+    {
+        try {
+            List1=new ArrayList<>();
+            JSONParser j = new JSONParser();
+            Map<String,Object> tasksListJson = 
+               j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+            
+            List<Map<String,Object>> list = (List<Map<String,Object>>)tasksListJson.get("root");
+            for(Map<String,Object> obj : list){
+                        JeuVideo jv = new JeuVideo();
+                        int id = (int) Float.parseFloat(obj.get("id").toString());
+                        jv.setId(id);
+                        String name = obj.get("namevg").toString();
+                        String image = obj.get("imagevg").toString();
+                        jv.setRating((int) Float.parseFloat(obj.get("rating").toString()));
+                        jv.setLikes((int) Float.parseFloat(obj.get("likes").toString()));
+                        jv.setNamevg(name);
+                        jv.setImagevg(image);
+                List1.add(jv);
+            }
+            
+            
+        } catch (IOException ex) {
+            
+        }
+        return List1;
+    }
+    
+    public ArrayList<JeuVideo> getAllJeu(){
+        req = new ConnectionRequest();
+        //String url = Statics.BASE_URL+"/tasks/";
+        String url = Statics.BASE_URL+"/Jeu/mobile/aff";
+        System.out.println("===>"+url);
+        req.setUrl(url);
+        req.setPost(false);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                List1 = parseJeu(new String(req.getResponseData()));
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return List1;
+    }
+    
+        public boolean deleteJV(int id) {
+        String url = Statics.BASE_URL + "/jeu/mobile/del?id=" + id;
+               req.setUrl(url);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                resultOK = req.getResponseCode() == 200; //Code HTTP 200 OK
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return resultOK;
+    }
+
+}
